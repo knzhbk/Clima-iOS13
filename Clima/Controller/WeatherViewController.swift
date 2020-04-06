333d7a6c59a8c0b4314399f59356831fa8183cab
Adina Kenzhebek <adina18022013@gmail.com>
2020-04-06 19:05:45 +0600

    Added URL session and Networking

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,47 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController,UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
     
+    var weatherManager = WeatherManager()
+    
+    @IBOutlet weak var searchTextField: UITextField!
     override func viewDidLoad() {
+        searchTextField.delegate = self
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func searchPressed(_ sender: UIButton) {
+        print(searchTextField.text!)
+        searchTextField.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print(searchTextField.text!)
+        searchTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if let city = searchTextField.text {
+            weatherManager.fetchWeather(cityName: city)
+            
+        }
+        searchTextField.text = ""
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type something"
+            return false
+            
+        }
+    }
 }
-
---
+++ Clima/Model/WeatherManager.swift
@@ -0,0 +1,46 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Adina Kenzhebekova on 4/3/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherManager {
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=d6c1e5e2fc21dbb6b2b641dcf84c8cf9&units=metric"
+    
+    func fetchWeather(cityName: String) {
+        let URLString = "\(weatherURL)&q=\(cityName)"
+        print(URLString)
+        performRequest(urlString: URLString)
+    }
+    
+    func performRequest(urlString: String) {
+        //1. Create URL
+        
+        if let url = URL(string: urlString) {
+            //2.Create a URL Session
+            let session = URLSession(configuration: .default)
+            
+            
+            //3.Give the session a task
+            
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    let dataString = String(data:safeData, encoding: .utf8)
+                    print(dataString!)
+                }
+            }
+            //4.Start the task
+            task.resume()
+            
+        }
+    }
+}
